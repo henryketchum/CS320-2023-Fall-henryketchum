ea707ddec28d951de49f96a7a1a8c57097c5bcac
henryketchum <henryk@bu.edu>
2023-09-27 17:38:24 -0400

    working more on assign2-1

--- assigns/assign2/MySolution/OCaml/assign2-1.ml
+++ assigns/assign2/MySolution/OCaml/assign2-1.ml
@@ -1,11 +1,13 @@
-#use "MyOCaml.ml";;
+#use "/home/henryk/CS320-2023-Fall/CS320-2023-Fall-henryketchum/assigns/assign2/assign2.ml"
+#use "/home/henryk/CS320-2023-Fall/CS320-2023-Fall-henryketchum/assigns/assign2/MySolution/OCaml/MyOCaml.ml";;
 
 let rec
 mylist_length(xs: 'a mylist): int =
   match xs with
-  | [] -> 0
-  | h :: t -> 1 + mylist_length(t)
-
+  | MyNil -> 0
+  (**| MyCons(x1,xs) -> 1 + mylist_length(MyNil)*)
+  | MyCons (_, xs) -> 1 + mylist_length xs
+;;
 
 let mylist_length =
 fun xs -> foreach_to_length(mylist_foreach)
